da68260061a0b8e6bec52b2c3bc85946dd3e8d45
wujunyu <1318543125@qq.com>
2016-06-17 19:08:04 +0800

    test

---
+++ MVS/src/edu/swjtu/model/Staff.java
@@ -0,0 +1,10 @@
+package edu.swjtu.model;
+
+public class Staff {
+	private int StaffId;
+	private String name;
+	private String number;
+	private String department;
+	private String group;
+	private int arrange_id;
+}
