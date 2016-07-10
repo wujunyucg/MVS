6891b5afc61eb821d3248e219bbb68b868c69df1
dell-pc <dell-pc@dell>
2016-07-10 14:22:48 +0800

    添加按number查找用户

--- MVS/src/edu/swjtu/impl/UserDaoImpl.java
+++ MVS/src/edu/swjtu/impl/UserDaoImpl.java
@@ -153,23 +153,4 @@ public class UserDaoImpl implements UserDao{
 		return user;
 	}
 
-	@Override
-	public User getUserByNumber(String userNumber, Connection con)
-			throws SQLException {
-		User user = null;
-		String sql = "select * from user where user_number=?";
-		try {
-			PreparedStatement pstm = con.prepareStatement(sql);
-			pstm.setString(1, userNumber);
-			ResultSet rs = pstm.executeQuery();
-			if(rs.next()){
-				user = getUserOne(rs);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return null;
-		}
-		return user;
-	}
-
 }
