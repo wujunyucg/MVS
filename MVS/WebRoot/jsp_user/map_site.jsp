5fa5d25754b39d919a67c8d54210492582ab1e7e
wujunyu <1318543125@qq.com>
2016-07-25 09:48:45 +0800

    导出站点

--- MVS/WebRoot/jsp_user/map_site.jsp
+++ MVS/WebRoot/jsp_user/map_site.jsp
@@ -199,67 +199,24 @@
 			</div>
 			<div id="myPageTop"
 				style="position: absolute; top:175px; right:100px;">
-				<div id="panel"></div>
 			</div>
-			<div id="addroute" style="position:absolute;margin-left:55px;margin-top:105px;background-color:#fff;width:180px;display:none">
-				<ul>
-					<li>路线<select size="1" id="addroute-number"></select></li>
-					<li id="addroute-start">起点<input type="text" value=""/></li>
-					<li id="addroute-end">终点<input type="text" value=""/></li>
-					<li >用时<span id="addroute-time">111</span></li>
-					<li >距离<span id="addroute-distance">111</span></li>
-					<li id="addroute-sbm"><button class="button-left">确认</button></li>
-					<li id="addroute-quit"><button class="button-right">取消</button></li>
-				</ul>
-			</div>
-			<div id="result" style="position:absolute;margin-left:55px;margin-top:105px;background-color:#fff;width:180px;display:none">		
-					<span id="routenumber",align="center" style="width:90%;margin:0px 10px;text-align:center">线路</span></br>	
-					<span id="start-end" style="width:90%;margin:0px 10px;">起点---终点</span>
-					<table style="width:90%;margin:0px 10px;">
-					<tr >
-						<td>时间</td>
-						<td>距离</td>
-					</tr>
-					<tr id="time-distance">
-						<td>155</td>
-						<td>155</td>
-					</tr>
-				</table>
-			</div>
-			<div>
-        <ul  id="routes" style="position: absolute;display:none;">
-            <li><button >线路1</button></li>
-            <li><button >线路2</button></li>
-            <li><button >线路3</button></li>
-        </ul>
-    </div>
-	<div style="position: absolute;margin-left:355px;margin-top:50px;">
-        <!--站点管理-->
-        <ul id="satation" style="display:none;">
-            <li>站点管理</li>
-            <ul >
-                <li><button onclick="addsatation()">添加站点</button></li>
-                <li>删除站点</li>
-                <li><button onclick="map.clearMap();markers(hhj_satation)">显示所有</button></li>
-                <li>查询站点</li>
-            </ul>
-        </ul>
-        <ul id="manage_route" style="display:none">
-            <li>路线管理</li>
-            <ul >
-                <li>添加路线</li>
-                <li>删除路线</li>
-                <li>详细信息</li>
-                <li>查询路线</li>
-            </ul>
-            <li><input id="lnglat" value=''/></li>
-        </ul>
-      </div>
       <div id="satation-search" style="position:absolute;margin-left:65px;margin-top:80px;width:180px" autoComplete='off'>
       	<input type="text" id="tipinput" value="输入关键字进行查询" />
       	
       </div>
       
+ <div class="dropdown" style="position:absolute;margin-left:20px;margin-top:580px;">
+  <button id="dLabel" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+   添加站点
+    <span class="caret"></span>
+  </button>
+
+  <ul class="dropdown-menu" aria-labelledby="dLabel">
+  <li><a>点击选点</a></li>
+     <li> <a> 搜索选点</a></li>
+  </ul>
+  
+</div>
       <div id="addsatation-info" style="position: absolute;margin-top:480px;display:none;">	 
          <ul id="info-satation" style="list-style-type:none;">
               <li>&nbsp;&nbsp;&nbsp&nbsp&nbsp&nbsp;&nbsp;名称&nbsp;<input type="text" value="" id="satation-name"/></li>
@@ -325,6 +282,9 @@
     <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
       <div class="panel-body" style="height: 200px">
       <c:if test="${site_list!=null }">
+     <form action="servlet/ExportSiteServlet" method="post">
+			<button type="submit" class="btn btn-primary">导出全部数据</button>
+		</form>
      	<table class="table  table-hover table-bordered " id="site_table">
 		<thead class="fixedThead">
 			<tr><th>#</th><th>站点名称</th>
@@ -397,10 +357,15 @@
 		//调节地图大小
 		var sitelist;
  			$("#site_table td").click(function() {
+ 			$("tr").each(function() {
+ 				$(this).css('background-color','white');
+ 			})
  				map.clearMap();
              var  tr=$(this).parent().attr("id");
                satationsmarker(sitelist[tr]);
                moveTocenter([sitelist[tr].longitude,sitelist[tr].latitude]);
+               $("#"+tr).css('background-color','red');
+        
            
             });
 		$(window).load(function(){
--- MVS/src/edu/swjtu/excel/ExportSiteServlet.java
+++ MVS/src/edu/swjtu/excel/ExportSiteServlet.java
@@ -1,37 +1,145 @@
 package edu.swjtu.excel;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import edu.swjtu.impl.CarDaoImpl;
+import edu.swjtu.impl.SiteDaoImpl;
+import edu.swjtu.model.Car;
+import edu.swjtu.model.Site;
+import edu.swjtu.util.DBUtil;
+
 /**
- * Servlet implementation class ExportSiteServlet
+ * 
+ * ExportSiteServlet.java类
+ * 2016年7月24日
+ * @author wujunyu
+ * TODo
  */
 public class ExportSiteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+   
     public ExportSiteServlet() {
         super();
-        // TODO Auto-generated constructor stub
+
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		doPost(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// 第一步，创建一个webbook，对应一个Excel文件
+				HSSFWorkbook wb = new HSSFWorkbook();
+				// 第二步，在webbook中添加一个sheet,对应Excel文件中的sheet
+				HSSFSheet sheet = wb.createSheet("站点信息表");
+				// 第三步，在sheet中添加表头第0行,注意老版本poi对Excel的行数列数有限制short
+				HSSFRow row = sheet.createRow((int) 0);
+				// 第四步，创建单元格，并设置值表头 设置表头居中
+				HSSFCellStyle style = wb.createCellStyle();
+				style.setAlignment(HSSFCellStyle.ALIGN_CENTER); // 创建一个居中格式
+
+				HSSFCell cell = row.createCell(0);
+				cell.setCellValue("序号");
+				cell.setCellStyle(style);
+				cell = row.createCell(1);
+				cell.setCellValue("站点名称");
+				cell.setCellStyle(style);
+				cell = row.createCell(2);
+				cell.setCellValue("站点地址");
+				cell.setCellStyle(style);
+				cell = row.createCell(3);
+				cell.setCellValue("站点人数");
+				cell.setCellStyle(style);
+				cell = row.createCell(4);
+				cell.setCellValue("站点所属线路");
+				cell.setCellStyle(style);
+				cell = row.createCell(5);
+				cell.setCellValue("站点经度");
+				cell.setCellStyle(style);
+				cell = row.createCell(6);
+				cell.setCellValue("站点纬度");
+				cell.setCellStyle(style);
+				cell = row.createCell(7);
+				cell.setCellValue("站点顺序");
+				cell.setCellStyle(style);
+				
+				// 第五步，写入实体数据 实际应用中这些数据从数据库得到，
+				Connection con = null;
+				DBUtil db = new DBUtil();
+				try {
+					con = db.getCon();
+				} catch (ClassNotFoundException | SQLException e1) {
+					e1.printStackTrace();
+				}
+				
+				ArrayList<Site> siteList = null;
+				siteList = new SiteDaoImpl().getAllSite(con);
+
+				for (int i = 0; i < siteList.size(); i++) {
+					row = sheet.createRow((int) i + 1);
+					Site site = (Site) siteList.get(i);
+					// 第四步，创建单元格，并设置值
+					row.createCell(0).setCellValue(i+1);
+					row.createCell(1).setCellValue(site.getName());
+					row.createCell(2).setCellValue(site.getAddress());
+					row.createCell(3).setCellValue(site.getPeoNum());
+					row.createCell(4).setCellValue(site.getLineId());
+					row.createCell(5).setCellValue(site.getLatitude());
+					row.createCell(6).setCellValue(site.getLongitude());
+					row.createCell(7).setCellValue(site.getOrder());
+				}
+
+				//根据路径下载
+//				FileOutputStream fout = new FileOutputStream("E:/car_data.xls");  
+//				wb.write(fout);  
+//				fout.close();  
+			
+				String myexcel="All_Site";
+			    //回去输出流
+			    OutputStream out=response.getOutputStream();
+			    //重置输出流
+			    response.reset();
+			    //设置导出Excel报表的导出形式
+			    response.setContentType("application/vnd.ms-excel");
+			    response.setHeader("Content-Disposition","attachment;filename="+myexcel+".xls");
+			    
+			    wb.write(out);
+			    out.close();
+			     
+			    //设置输出形式
+			    System.setOut(new PrintStream(out));
+			    //刷新输出流
+			    out.flush();
+			    //关闭输出流
+			    if(out!=null){
+			      out.close();
+			    }
+			    
+			    try {
+					db.closeCon(con);
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 	}
 
 }
