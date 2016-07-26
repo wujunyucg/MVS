04138a97141c414d7bbf8c29be59b2a970b404fc
dell-pc <dell-pc@dell>
2016-07-26 13:25:20 +0800

    路线管理部分功能

--- MVS/src/edu/swjtu/dao/LineDaoTest.java
+++ MVS/src/edu/swjtu/dao/LineDaoTest.java
@@ -1,6 +1,7 @@
 package edu.swjtu.dao;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import org.junit.Test;
 
@@ -39,11 +40,30 @@ public class LineDaoTest {
 	@Test
 	public void testUpdate() throws ClassNotFoundException, SQLException{
 		Line line = new Line();
-		line.setCarId(1);
+		line.setCarId("1");
 		line.setLineId(1);
 		line.setName("线路1");
 		line.setNum(40);
 		line.setSiteId("1,2,3");
 		System.out.println(new LineDaoImpl().updateLine(new DBUtil().getCon(),line));
+	}
+	
+	@Test
+	public void testAddMore() throws ClassNotFoundException, SQLException{
+		ArrayList<Line> linelist = new ArrayList<Line>();
+		for(int i=0;i<3;i++){
+			Line line = new Line();
+			linelist.add(line);
+			linelist.get(i).setCarId(i + ",5");
+			linelist.get(i).setName("线路" + i + 1);
+			linelist.get(i).setNum(40 + i);
+			linelist.get(i).setSiteId("1,2,3" + "," + i);
+			linelist.get(i).setRate((i+5) / 5.1 * 1.0);
+		}
+		System.out.println(new LineDaoImpl().addMoreLine(linelist, new DBUtil().getCon()));
+	}
+	@Test
+	public void testDeleteAll() throws ClassNotFoundException, SQLException{
+		System.out.println(new LineDaoImpl().deleteAllLine( new DBUtil().getCon()));
 	}
 }
