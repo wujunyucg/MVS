eeb711a77f54d2988661ee3d14835c1c65af9500
jimolonely <15982406684@163.com>
2016-07-30 10:49:22 +0800

    修改排班bug

--- MVS/src/edu/swjtu/servlet/ManageArrangeServlet.java
+++ MVS/src/edu/swjtu/servlet/ManageArrangeServlet.java
@@ -47,7 +47,7 @@ public class ManageArrangeServlet extends HttpServlet {
 		int startPage = 1;
 		int pageNum = 10;
 
-		//type==1查看所有班次，==2按月查看,==3代表导入的文件解析结果
+		// type==1查看所有班次，==2按月查看,==3代表导入的文件解析结果
 		String sp = request.getParameter("arrStartPage");
 		String type = request.getParameter("type");
 
@@ -60,9 +60,10 @@ public class ManageArrangeServlet extends HttpServlet {
 		int total = 0;
 		int total_page = 1;
 
-		System.out.println("type="+type);
-		boolean isUpload = false;//判断是否是文件上传的数据
-		String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date()).substring(0, 7);
+		System.out.println("type=" + type);
+		boolean isUpload = false;// 判断是否是文件上传的数据
+		String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date())
+				.substring(0, 7);
 		try {
 			HttpSession session = request.getSession();
 			con = db.getCon();
@@ -70,42 +71,43 @@ public class ManageArrangeServlet extends HttpServlet {
 			CarDaoImpl cdi = new CarDaoImpl();
 			LineDaoImpl ldi = new LineDaoImpl();
 			ArrayList<Arrange> arrList = null;
-			if(type.equals("3")){
+			if (type.equals("3")) {
 				FileUpload fl = new FileUpload();
 				int re = fl.upload(request);
 				String path = fl.getAllPath();
-				System.out.println("path:"+path);
+				System.out.println("path:" + path);
 				ArrayList<ArrCarLine> al = new InportArrExcel().inport(path);
-				System.out.println(al.size()+"=size");
-				
-				///解析并存到数据库
-				//////////////////////////////
-				
-				for(ArrCarLine acl : al){
-					
+				System.out.println(al.size() + "=size");
+
+				// /解析并存到数据库
+				// ////////////////////////////
+
+				for (ArrCarLine acl : al) {
+
 					Arrange arr = new Arrange();
 					arr.setDate(acl.getDate());
 					arr.setTime(acl.getTime());
 					arr.setName(acl.getArrName());
-					Car car = cdi.getCarByLicensePlate(acl.getLicensePlate(), con);
-					if(null==car){
+					Car car = cdi.getCarByLicensePlate(acl.getLicensePlate(),
+							con);
+					if (null == car) {
 						arr.setCarId(1);
-					}else{
+					} else {
 						arr.setCarId(car.getCarId());
 					}
 					Line line = ldi.getLineByName(con, acl.getLineName());
-					if(null==line){
+					if (null == line) {
 						arr.setLineId(1);
-					}else{
+					} else {
 						arr.setLineId(line.getLineId());
 					}
 					adi.addArr(arr, con);
 				}
-				//解析完删除文件
+				// 解析完删除文件
 				fl.delOneFile(path);
 				response.getWriter().write("yes");
-			}else{
-				
+			} else {
+
 				if (type.equals("1")) {
 					date = "all";
 					arrList = adi.getPageArrange(con, startPage, pageNum);
@@ -123,9 +125,11 @@ public class ManageArrangeServlet extends HttpServlet {
 				}
 				ArrayList<ArrCarLine> arrData = new ArrayList<ArrCarLine>();
 				for (Arrange arr : arrList) {
-					arrData.add(new ArrCarLine(arr, cdi.getCarById(
-							arr.getCarId(), con), ldi.getLineById(con,
-							arr.getLineId())));
+					Line l = ldi.getLineById(con, arr.getLineId());
+					Car c = cdi.getCarById(arr.getCarId(), con);
+					if (null != l && null != c) {
+						arrData.add(new ArrCarLine(arr, c, l));
+					}
 				}
 				session.setAttribute("arr_date", date);
 				session.setAttribute("arr_type", type);
@@ -151,5 +155,4 @@ public class ManageArrangeServlet extends HttpServlet {
 			}
 		}
 	}
-
 }
